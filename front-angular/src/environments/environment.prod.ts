6a657aa4dbf9038cc21ef2b612a7e984aa7f4902
Mossab <mahat.mossab@gmail.com>
2021-01-24 02:20:27 +0100

    Task: update angular environment.prod.ts

--- front-angular/src/environments/environment.prod.ts
+++ front-angular/src/environments/environment.prod.ts
@@ -2,18 +2,18 @@ import { KeycloakConfig } from 'keycloak-angular';
 
 // Add here your keycloak setup infos
 let keycloakConfig: KeycloakConfig = {
-  url: 'http://localhost:8180/auth',
-  realm: 'local',
+  url: 'https://auth.maxilog.tech/auth',
+  realm: 'poc',
   clientId: 'front'
 };
 
 export const environment = {
   production: false,
   apis: {
-    customer: 'http://localhost:8080/customer',
-    notification: 'http://localhost:8080/notification',
-    product: 'http://localhost:8080/product',
-    order: 'http://localhost:8080/order'
+    customer: 'https://poc-spring.maxilog.tech/customer',
+    notification: 'https://poc-spring.maxilog.tech/notification',
+    product: 'https://poc-spring.maxilog.tech/product',
+    order: 'https://poc-spring.maxilog.tech/order'
   },
   keycloak: keycloakConfig
 };
