2cae873471060615b6dc197b528cc5ee7f2b906d
Mossab <mahat.mossab@gmail.com>
2021-02-12 15:26:04 +0100

    Task: Config Spring Cloud Kubernetes

--- gateway/zuul/src/main/java/io/maxilog/GatewayApplication.java
+++ gateway/zuul/src/main/java/io/maxilog/GatewayApplication.java
@@ -1,13 +1,19 @@
 package io.maxilog;
 
+import io.maxilog.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+@AutoConfigureAfter(RibbonAutoConfiguration.class)
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
