85b6f9869f9423d5bad2ec3e1de3d3d7caf9477f
Mossab <mahat.mossab@gmail.com>
2021-02-15 16:22:31 +0100

    Task: Config Spring Cloud Kubernetes

--- notification-service/src/main/java/io/maxilog/NotificationApplication.java
+++ notification-service/src/main/java/io/maxilog/NotificationApplication.java
@@ -2,23 +2,14 @@ package io.maxilog;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.schema.registry.client.EnableSchemaRegistryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.integration.core.MessagingTemplate;
-import org.springframework.messaging.core.MessageSendingOperations;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
 
 @EnableDiscoveryClient
-@EnableSchemaRegistryClient
 @SpringBootApplication
 public class NotificationApplication {
 
@@ -30,32 +21,12 @@ public class NotificationApplication {
         this.env = env;
     }
 
-    @PostConstruct
-    public void initApplication() {
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
-    @Autowired
-    MessageSendingOperations messagingTemplate;
-    Map<String, Integer> progress = new HashMap<>();
-
-    //@Bean
-    public CommandLineRunner websocketDemo() {
-        return (args) -> {
-            while (true) {
-                try {
-                    Thread.sleep(3*1000); // Each 3 sec.
-                    progress.put("num1", 100);
-                    progress.put("num1", 100);
-                    messagingTemplate.convertAndSend("/notifications", this.progress);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+    @PostConstruct
+    public void initApplication() {
     }
 
 }
